0e42c73b33adf86e6af813d72f0560c5babf48ad
krp97 <k.plata576@gmail.com>
2019-10-25 15:39:52 +0200

    Data & repo init

---
+++ data/data.py
@@ -0,0 +1,26 @@
+
+# Lists containing names of features and classes.
+# All names are sorted according to their appearance in the source xls file.
+
+
+STP_FEATURES = ['Płeć', 'Wiek', 'Lokalizacja bólu na początku zachorowania',
+                'Lokalizacja bólu obecnie', 'Intensywność bólu',
+                'Czynniki nasilające ból', 'Czynniki przynoszące ulgę',
+                'Progresja bólu', 'Czas trwania bólu',
+                'Charakter bólu na początku zachorowania',
+                'Charakter bólu obecnie', 'Nudności i wymioty',
+                'Apetyt', 'Wypróżnienia', 'Oddawanie moczu',
+                'Poprzednie niestrawności', 'Żółtaczka w przeszłości',
+                'Poprzednie operacje brzuszne', 'Leki', 'Stan psychiczny',
+                'Skóra', 'Temperatura(pacha)', 'Tętno',
+                'Ruchy oddechowe powłok brzusznych', 'Wzdęcia',
+                'Umiejscowienie bolesności uciskowej',
+                'Objaw Blumberga', 'Obrona mięśniowa',
+                'Wzmożone napięcie powłok brzusznych',
+                'Opory patologiczne', 'Objaw Murphy\'ego']
+
+STP_CLASSES = ['Ostre zapalenie wyrostka robaczkowego',
+               'Zapalenie uchyłków jelit', 'Niedrożnośc mechaniczna jelit',
+               'Perforowany wrzód trawienny', 'Zapalenie woreczka żółciowego',
+               'Ostre zapalenie trzustki', 'Niecharakterystyczny ból brzucha',
+               'Inne przyczyny ostrego bólu brzucha']
