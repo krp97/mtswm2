e5b630ae0e8ff01c04050e5c82ee836c539afd60
krp97 <k.plata576@gmail.com>
2019-11-08 12:32:40 +0100

    Docstrings for the experiment

--- main.py
+++ main.py
@@ -17,13 +17,24 @@ def basic_rank():
 
 
 def run_experiment(x, y, upper_bound):
+    '''
+    Runs the experiment 'upper_bound' amount of times.
+    Each iteration, another feature from the ranking gets added to the grid search process.
+    Results of each iteration are dumped into a latex table.
+
+    Parameters:
+        x (pandas dataframe): Dataframe obtained from loading a csv file.
+        y (numpy array): Array of class id's for each row in a dataframe.
+        upper_bound (int): Last amount of features before the experiment exits.
+    '''
     results = []
     for i in range(1, upper_bound):
         new_x, new_y = fr.rank_n_transform(x, y, i)
         partial_res = clsf.run_grid_search(new_x, new_y)
         avg_test = clsf.get_avg_test_score(partial_res)
         results.append((i, i, avg_test))
-    clsf.dump_to_latex(results, ['L.P.', 'Liczba cech', 'Jakość klasyfikacji [\%]'])
+    clsf.dump_to_latex(
+        results, ['L.P.', 'Liczba cech', 'Jakość klasyfikacji [\%]'])
 
 
 def main():
