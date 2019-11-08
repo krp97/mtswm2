8dbf270415c44eb15216f05e7d5320f7aad30614
krp97 <k.plata576@gmail.com>
2019-11-08 12:09:06 +0100

    Cleaned up the estimator script

--- classifier.py
+++ classifier.py
@@ -1,17 +1,49 @@
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import GridSearchCV, RepeatedKFold
 import feature_ranking as fr
+import os
+from tabulate import tabulate
 
-data, classes = fr.load_data('./data/brzuch_dane.csv')
 
-param_grid = {
-    "solver": ["sgd", "adam"],
-    "hidden_layer_sizes": [(10,), (15,), (20,)]
-}
-rkf = RepeatedKFold(n_splits=2, n_repeats=5)
+def run_grid_search(data, classes):
+    '''
+    Returns a dictionary with the results of a grid search over the following parameters:
+        solver - [sgd, adam]; momentum / no-momentum
+        hidden_layer_sizes - [10, 15, 20]
+    The estimator is evaluated using 5 times repeated 2 fold cv.
+    Parameters:
+        data (pandas dataframe): Dataframe of feature values obtained from loading a csv file.
+        classes (numpy array): Array of class id's for each row in a dataframe.
+    '''
+    param_grid = {
+        "solver": ["sgd", "adam"],
+        "hidden_layer_sizes": [(10,), (15,), (20,)]
+    }
+    rkf = RepeatedKFold(n_splits=2, n_repeats=5)
 
-gs = GridSearchCV(MLPClassifier(), param_grid,
-                  scoring='accuracy', n_jobs=-1, cv=rkf)
+    gs = GridSearchCV(MLPClassifier(), param_grid,
+                      scoring='accuracy', n_jobs=-1, cv=rkf)
 
-gs.fit(data, classes)
-print(gs.cv_results_)
+    gs.fit(data, classes)
+    return gs.cv_results_
+
+
+def dump_to_latex(values, headers, filename=None):
+    '''
+    Dumps a list of tuples into a latex table.
+    Parameters:
+        results  (list of tuples): List of values for each row in a table.
+        headers  (list): List of table header names.
+        filename (str): File path for saving the output. If none, the output is redirected to stdout.
+    '''
+    table = [list(row) for row in values]
+    latex_out = tabulate(table, tablefmt="latex_raw", headers=headers)
+    if filename:
+        with open(filename, "w+") as f:
+            f.write(latex_out)
+    else:
+        print(latex_out)
+
+# Example usage:
+# values = [(10, 'abc', 102.12), (20, 'xd', 102.12)]
+# dump_to_latex(values, ['xd', 'xd', 'xd'], 'output.txt')
--- feature_ranking.py
+++ feature_ranking.py
@@ -21,17 +21,32 @@ def load_data(csv_path):
     return (data, classes)
 
 
+def rank_n_transform(data, classes, feat_count):
+    '''
+    Returns a tuple with the transformed feature vector and input class_id vector.
+
+    Parameters:
+        data (pandas dataframe): Dataframe obtained from loading a csv file.
+        classes (numpy array): Array of class id's for each row in a dataframe.
+        feat_count (int): Determines how many features will be selected in KBest transformation.
+    '''
+    kb_selector = SelectKBest(score_func=chi2, k=feat_count)
+    data_new = kb_selector.fit_transform(data, classes)
+    return (data_new, classes)
+
+
 def rank_features(data, classes, sort=True):
     '''
     Returns a dictionary or a list of tuples sorted by value, 
     where the first element is the feature_id and the second
-    is it's ranking score.
+    is it's ranking score. The k parameter specifies how many features should be returned.
 
     Parameters:
         data (pandas dataframe): Dataframe obtained from loading a csv file.
-        classes (numpy array): Array of class id's for each row in a dataframe. 
+        classes (numpy array): Array of class id's for each row in a dataframe.
+        feat_count (int): Feature count in the destination dictionary.
     '''
-    kb_selector = SelectKBest(score_func=chi2, k=data.shape[1])
+    kb_selector = SelectKBest(score_func=chi2, k=data[0].shape)
     kb_selector.fit(data, classes)
     results = kb_selector.scores_
     output = dict(zip(data.columns, results))
