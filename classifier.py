44b067009c87a5c065629c300dd585afc073442d
krp97 <k.plata576@gmail.com>
2019-11-11 21:20:30 +0100

    Fixed bad scoring & table creation

--- classifier.py
+++ classifier.py
@@ -28,35 +28,18 @@ def run_grid_search(data, classes):
     return gs.cv_results_
 
 
-def get_avg_test_score(grid_out, dec=2):
-    '''
-    Returns the average of 'mean_test_score' from a grid_search run.
-
-    Parameters:
-        grid_out (dict): Output from a grid search run.
-        dec (int): Specifiy the decimal place alignment of the output value.
-    '''
-    value = grid_out['mean_test_score']
-    percentage = (sum(value)/len(value))*100
-    return float("{0:.2f}".format(percentage))
-
-
-def dump_to_latex(values, headers, filename=None):
+def pretty_print_cv(values, headers, texfile=None):
     '''
     Dumps a list of tuples into a latex table.
     Parameters:
         results  (list of tuples): List of values for each row in a table.
         headers  (list): List of table header names.
-        filename (str): File path for saving the output. If none, the output is redirected to stdout.
+        texfile (str): File path for saving the output. If none, the output is redirected to stdout.
     '''
     table = [list(row) for row in values]
-    latex_out = tabulate(table, tablefmt="latex_raw", headers=headers)
-    if filename:
-        with open(filename, "w+") as f:
+    if texfile:
+        latex_out = tabulate(table, tablefmt="latex_raw", headers=headers)
+        with open(texfile, "w+") as f:
             f.write(latex_out)
     else:
-        print(latex_out)
-
-# Example usage:
-# values = [(10, 'abc', 102.12), (20, 'xd', 102.12)]
-# dump_to_latex(values, ['xd', 'xd', 'xd'], 'output.txt')
+        print(tabulate(table, tablefmt="grid", headers=headers))
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+import warnings
 import sys
 import os
 import feature_ranking as fr
@@ -16,7 +17,30 @@ def basic_rank():
     fr.pretty_print_ranking(ranking)
 
 
-def run_experiment(x, y, upper_bound):
+def parse_into_table(results):
+    '''
+    Parses the experiment output into table format.
+    Returns a 2 dimensional list of values.
+
+    Parameters:
+        results (list(tuple(dict, int))): Output retrieved from running 'run_experiment'.
+    '''
+    output = []
+    for res, feature_count in results:
+        for item, param in enumerate(res['params']):
+            row = [feature_count]
+            layer_size, = param['hidden_layer_sizes'] # --> note: hidden_layer_sizes is a tuple
+            row.append(layer_size)
+            row.append(
+                'Tak') if param['solver'] == 'sgd' else row.append('Nie')
+            avg_score = res['mean_test_score'][item] * 100
+            row.append("{0:.2f}".format(avg_score))
+            output.extend([row])
+
+    return output
+
+
+def run_experiment(x, y, upper_bound=5):
     '''
     Runs the experiment 'upper_bound' amount of times.
     Each iteration, another feature from the ranking gets added to the grid search process.
@@ -31,16 +55,17 @@ def run_experiment(x, y, upper_bound):
     for i in range(1, upper_bound):
         new_x, new_y = fr.rank_n_transform(x, y, i)
         partial_res = clsf.run_grid_search(new_x, new_y)
-        avg_test = clsf.get_avg_test_score(partial_res)
-        results.append((i, i, avg_test))
-    clsf.dump_to_latex(
-        results, ['L.P.', 'Liczba cech', 'Jakość klasyfikacji [\%]'])
+        results.append((partial_res, i))
+
+    table_data = parse_into_table(results)
+    headers = ['Liczba cech', 'Liczba neuronów', 'Momentum', 'Skuteczność [\%]']
+    clsf.pretty_print_cv(table_data, headers)
 
 
 def main():
     csv_path = os.path.join(get_script_path(), 'data/brzuch_dane.csv')
     x, y = fr.load_data(csv_path)
-    run_experiment(x, y, 10)
+    run_experiment(x, y, upper_bound=3)
 
 
 if __name__ == '__main__':
